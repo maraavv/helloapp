ee2a0e848d96890281698694a04d3932170da168
black <black@example.com>
2026-07-04 11:55:05 +0500

    Call greetBlack

---
+++ hello-black.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello everyone");
+    }
+
+    static void GreetWhite()
+    {
+        Console.WriteLine("hello white");
+    }
+
+    static void GreetBlack()
+    {
+        Console.WriteLine("hello black");
+    }
+}
